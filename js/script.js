0e787dfbf0dc0132f0a4b08f069cf4709011e978
David <david.auler@gmail.com>
2015-12-29 22:56:49 -0500

    Added a main header, clicking a team member's name now populates the left side with their name and a video, and altered various fonts.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,16 @@ function addMembers() {
 		id: 'FritzyP',
 		click: populate
 	}).append('FritzyP').appendTo('#members');
+
+	$('<li></li>', {
+		id: 'StoicAetos',
+		click: populate
+	}).append('StoicAetos').appendTo('#members');
+
+	$('<li></li>', {
+		id: 'Thrompy',
+		click: populate
+	}).append('Thrompy').appendTo('#members');
 }
 
 function populate() {
@@ -18,7 +28,43 @@ function populate() {
 	$('section').empty();
 	if (clickedID == 'FritzyP') {
 		$('<p></p>', {
-			id: 'test'
-		}).append('It worked!').appendTo('section');
+			id: 'contentHeader'
+		}).append('FritzyP').appendTo('section');
+
+	$('<iframe></iframe>', {
+			id: 'montageVid',
+			width:'560', 
+			height:'315', 
+			src:'https://www.youtube.com/embed/5LiZesUDIfE', 
+			frameborder:'0'
+		}).appendTo('section');
+	}
+
+	if (clickedID == 'StoicAetos') {
+		$('<p></p>', {
+			id: 'contentHeader'
+		}).append('StoicAetos').appendTo('section');
+
+		$('<iframe></iframe>', {
+			id: 'montageVid',
+			width:'560', 
+			height:'315', 
+			src:'https://www.youtube.com/embed/dJ_OoZu2pyI', 
+			frameborder:'0'
+		}).appendTo('section');
+	}
+
+	if (clickedID == 'Thrompy') {
+		$('<p></p>', {
+			id: 'contentHeader'
+		}).append('Thrompy').appendTo('section');
+
+	$('<iframe></iframe>', {
+			id: 'montageVid',
+			width:'560', 
+			height:'315', 
+			src:'https://www.youtube.com/embed/64kAeHVxiM8', 
+			frameborder:'0'
+		}).appendTo('section');	
 	}
 }
